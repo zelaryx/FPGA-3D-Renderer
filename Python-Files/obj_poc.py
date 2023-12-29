dc7cee9dece908e83e3a12110fb72221906d5f03
zelaryx <alyssazihuihuang@gmail.com>
2023-12-29 04:33:31 -0800

    reorganizing

---
+++ Python-Files/obj_poc.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.path)
+# from turtle import Turtle
+# from random import random
+
+# t = Turtle()
+# for i in range(100):
+#     steps = int(random() * 100)
+#     angle = int(random() * 360)
+#     t.right(angle)
+#     t.fd(steps)
+
+# t.screen.mainloop()
